8db95da8a179707c212ec19385dfd42284e8774d
Ali Moallem <AliMoallem27@gmail.com>
2021-03-16 14:19:26 +0330

    add button animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { ReactSVG } from "react-svg";
+// import { ReactSVG } from "react-svg";
+import Lottie from "react-lottie";
+import animationData from "../assets/lottie/add.json";
 
 import useSetCartData from "../hooks/useSetCartData";
 
-import addIco from "../assets/images/svg/add.svg";
+// import addIco from "../assets/images/svg/add.svg";
 
 function Card({ name, price, number, picture, id }) {
-  const setCartData = useSetCartData();
-  // SHOULD REFACTOOOOOOOOOOOOOOOOOOOR
-  // console.log("number: " + number);
-
   const [displayNumber, setDisplayNumber] = useState(number);
+  const [playAnimation, setPlayAnimation] = useState(true);
+  const setCartData = useSetCartData();
 
   const handleAddCart = () => {
     if (displayNumber > 0) {
       setCartData({ name, price, numberAdded: 1, picture, id });
       setDisplayNumber((pre) => pre - 1);
+      setPlayAnimation(false);
+      setTimeout(() => {
+        setPlayAnimation(true);
+      }, 700);
     }
   };
 
   useEffect(() => {
     setDisplayNumber(number);
   }, [number]);
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: false,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   return (
     <article className="rounded-xl shadow-lg w-10/12 md:w-full h-72">
@@ -31,10 +44,9 @@ function Card({ name, price, number, picture, id }) {
         </figure>
       </header>
       <footer className="w-full px-3 pt-2 flex flex-col relative h-1/4">
-        <div onClick={handleAddCart} className="absolute -top-5 left-6 w-10 h-10 rounded-full bg-cupcake-light flex items-center justify-center cursor-pointer">
-          <div className="w-5 h-5">
-            <ReactSVG src={addIco} />
-          </div>
+        <div onClick={handleAddCart} className="absolute -top-9 left-2 w-15 h-15 rounded-full flex items-center justify-center cursor-pointer">
+          {/* <ReactSVG src={addIco} /> */}
+          <Lottie options={defaultOptions} height={70} width={70} isStopped={playAnimation} />
         </div>
         <h3 className="text-gray-700 mb-2">{name}</h3>
         <div className="flex justify-between">
