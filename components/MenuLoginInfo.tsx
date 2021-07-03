1fdeaa4dbb9e2a34188938f539239855ad6bda81
Ali Moallem <AliMoallem27@gmail.com>
2021-07-04 01:36:48 +0430

    Add MenuLoginInfo

---
+++ components/MenuLoginInfo.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Btn from "./Btn";
+import useAuth from "../hooks/useAuth";
+
+export default function MenuLoginInfo() {
+  const { open } = useAuth();
+
+  return (
+    <div className="overflow-auto">
+      <div className="text-right w-full whitespace-no-wrap rounded-lg text-sm md:text-base text-gray-600 hover:text-gray-800">
+        <div className="flex flex-col px-2 py-2 h-full">
+          <div className="flex flex-col items-center">
+            <div className="w-24 my-6">
+              <Image src="/images/icons/smile.png" height={100} width={100} alt="Sad emoji" />
+            </div>
+            <span>لطفا وارد شوید یا ثبت نام کنید</span>
+          </div>
+        </div>
+      </div>
+      <div className="pt-2 border-t-2 border-gray-100 flex justify-between">
+        <Btn onClick={() => open()} text="ورود" type="primary" size="half" />
+        <Btn onClick={() => open()} text="ثبت نام" type="secondary" size="half" />
+      </div>
+    </div>
+  );
+}
