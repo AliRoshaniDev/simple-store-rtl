c427e82f46c19233cedb6719a440f48aa9f8b82d
Ali Moallem <AliMoallem27@gmail.com>
2021-07-07 13:56:05 +0430

    Reform useCartTotal hook

--- components/MenuFillCartTotal.tsx
+++ components/MenuFillCartTotal.tsx
@@ -5,7 +5,7 @@ import Btn from "./Btn";
 
 function MenuFillCartTotal() {
   const { deleteAll } = useSetCartData();
-  const calcualteTotal = useCartTotal();
+  const { totalNumber, totalPrice } = useCartTotal();
 
   const handleAddCart = (event: React.MouseEvent<HTMLElement>) => {
     event.stopPropagation();
@@ -17,11 +17,11 @@ function MenuFillCartTotal() {
       <div className="flex flex-col px-2 py-2 h-full border-t-2 border-b-2 border-gray-100">
         <div className="flex justify-between mb-1">
           <span className="font-semibold">جمع قیمت:</span>
-          <span className="text-gray-600">{calcualteTotal("price")} ریال</span>
+          <span className="text-gray-600">{totalPrice} ریال</span>
         </div>
         <div className="flex justify-between">
           <span className="font-semibold">جمع تعداد:</span>
-          <span className="text-gray-600">{calcualteTotal("number")} عدد</span>
+          <span className="text-gray-600">{totalNumber} عدد</span>
         </div>
       </div>
       <div className="pt-2">
--- hooks/useCartTotal.ts
+++ hooks/useCartTotal.ts
@@ -3,14 +3,10 @@ import useCartData from "../hooks/useCartData";
 export default function useCartTotal() {
   const cartData = useCartData();
 
-  function calcualteTotal(requestedData: "number" | "price") {
-    let total = 0;
-    for (let i = 0; i < cartData!.length; i++) {
-      if (requestedData === "number") total += cartData![i].numberAdded;
-      if (requestedData === "price") total += cartData![i].price;
-    }
-    return total;
-  }
-
-  return calcualteTotal;
+  return cartData!.reduce(
+    (total, item) => {
+      return { totalNumber: total.totalNumber + item.numberAdded, totalPrice: total.totalPrice + item.price };
+    },
+    { totalNumber: 0, totalPrice: 0 }
+  );
 }
