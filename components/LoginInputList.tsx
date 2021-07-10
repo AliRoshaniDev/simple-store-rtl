ecad3610d4945a8dfa96c4f67fdfc442e9dd416f
Ali Moallem <AliMoallem27@gmail.com>
2021-07-11 01:13:14 +0430

    Update LoginInputList

--- components/LoginInputList.tsx
+++ components/LoginInputList.tsx
@@ -1,18 +1,31 @@
-import { Dispatch, SetStateAction } from "react";
-import { LoginDataTypes } from "../types/index";
+import { ChangeEvent } from "react";
+import Btn from "./Btn";
+import useAuth from "../hooks/useAuth";
 
-export default function LoginInputList(props: { setLoginInputs: Dispatch<SetStateAction<LoginDataTypes>> }) {
-  const { setLoginInputs } = props;
+export default function LoginInputList() {
+  const { login } = useAuth();
+
+  const handleSubmitForm = (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const emailValue = (event.target.children[1] as HTMLInputElement).value;
+    const passwordValue = (event.target.children[3] as HTMLInputElement).value;
+
+    login(emailValue, passwordValue);
+  };
+
   return (
-    <>
+    <form onSubmit={(event) => handleSubmitForm(event as ChangeEvent<HTMLFormElement>)}>
       <label htmlFor="email-input" className="ml-auto mb-1">
         ایمیل:
       </label>
-      <input type="email" dir="ltr" id="email-input" onChange={(event) => setLoginInputs((preState) => ({ ...preState, email: event.target.value }))} className="border-2 border-gray-300 focus:border-gray-400 w-full h-10 rounded-lg font-vazir-latin mb-2 p-2 outline-none" />
+      <input type="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" title="لطفا مقدار ایمیل را به درستی وارد نمایید" dir="ltr" id="email-input" className="border-2 border-gray-300 focus:border-gray-400 w-full h-10 rounded-lg font-vazir-latin mb-2 p-2 outline-none" />
       <label htmlFor="password-input" className="ml-auto mb-1">
         رمز عبور:
       </label>
-      <input type="password" dir="ltr" id="password-input" onChange={(event) => setLoginInputs((preState) => ({ ...preState, password: event.target.value }))} className="border-2 border-gray-300 focus:border-gray-400 w-full h-10 rounded-lg font-vazir-latin p-2 outline-none" />
-    </>
+      <input type="password" required id="password-input" title="لطفا رمز عبور را وارد نمایید" dir="ltr" className="border-2 border-gray-300 focus:border-gray-400 w-full h-10 rounded-lg font-vazir-latin p-2 outline-none" />
+      <div className="pt-2 border-t-2 border-gray-100 flex justify-between">
+        <Btn text="ورود" type="primary" size="w-full" />
+      </div>
+    </form>
   );
 }
