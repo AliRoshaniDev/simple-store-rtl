aac0114d6e3654d8d4f0581ffc744e06f75dba1c
Ali Moallem <AliMoallem27@gmail.com>
2021-07-11 01:15:03 +0430

    Update MenuAuth

--- components/MenuAuth.tsx
+++ components/MenuAuth.tsx
@@ -3,13 +3,9 @@ import Btn from "./Btn";
 import useAuth from "../hooks/useAuth";
 import EmojiAndText from "./EmojiAndText";
 import LoginInputList from "./LoginInputList";
-import { LoginDataTypes, SignupDataTypes } from "../types/index";
 import SignupInputList from "./SignupInputList";
 
 export default function MenuAuth() {
-  const [loginInputs, setLoginInputs] = useState<LoginDataTypes>({ email: "null", password: "null" });
-  const [signupInputs, setSignupInputs] = useState<SignupDataTypes>({ email: "null", password: "null", data: { full_name: "null" } });
-
   const { login, signup } = useAuth();
   const [userStatus, setUserStatus] = useState<"login" | "signup" | null>(null);
 
@@ -20,15 +16,17 @@ export default function MenuAuth() {
           <div className="flex flex-col items-center">
             {userStatus === null && <EmojiAndText text="لطفا وارد شوید یا ثبت نام کنید" emoji="/images/icons/smile.png" />}
             {userStatus !== null && <Btn onClick={() => setUserStatus(null)} icon="/images/..." type="secondary" size="w-10" optionalStyle="mr-auto" noBorder={true} />}
-            {userStatus === "login" && <LoginInputList setLoginInputs={setLoginInputs} />}
-            {userStatus === "signup" && <SignupInputList setSignupInputs={setSignupInputs} />}
+            {userStatus === "login" && <LoginInputList />}
+            {userStatus === "signup" && <SignupInputList />}
           </div>
         </div>
       </div>
-      <div className="pt-2 border-t-2 border-gray-100 flex justify-between">
-        {userStatus !== "signup" && <Btn onClick={userStatus === null ? () => setUserStatus("login") : () => login(loginInputs.email, loginInputs.password)} text="ورود" type="primary" size={userStatus === "login" ? "w-full" : "w-1/2-less"} />}
-        {userStatus !== "login" && <Btn onClick={userStatus === null ? () => setUserStatus("signup") : () => signup(signupInputs.email, signupInputs.password, signupInputs.data)} text="ثبت نام" type="secondary" size={userStatus === "signup" ? "w-full" : "w-1/2-less"} />}
-      </div>
+      {userStatus === null && (
+        <div className="pt-2 border-t-2 border-gray-100 flex justify-between">
+          <Btn onClick={() => setUserStatus("login")} text="ورود" type="primary" size="w-1/2-less" />
+          <Btn onClick={() => setUserStatus("signup")} text="ثبت نام" type="secondary" size="w-1/2-less" />
+        </div>
+      )}
     </div>
   );
 }
