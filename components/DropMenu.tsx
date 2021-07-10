7df95a211550655936cf5914a2a0fd04285c5bda
Ali Moallem <AliMoallem27@gmail.com>
2021-07-10 17:00:56 +0430

    Update DropMenu

--- components/DropMenu.tsx
+++ components/DropMenu.tsx
@@ -1,14 +1,14 @@
 import { ChildrenType, DropMenuProps } from "../types/index";
 
 export default function DropMenu(props: ChildrenType & DropMenuProps) {
-  const { children, xAdjustment, widthAdjustment } = props;
+  const { children, hidden, xAdjustment, widthAdjustment } = props;
 
   //   case "filter":
   //     xAdjustment = "top-10 -right-3";
   //     widthAdjustment = "w-max";
 
   return (
-    <div dir="rtl" className={`absolute ${xAdjustment} ${widthAdjustment} p-2 rounded-lg shadow-lg bg-white z-10`}>
+    <div dir="rtl" hidden={hidden} className={`absolute ${xAdjustment} ${widthAdjustment} p-2 rounded-lg shadow-lg bg-white z-10 cursor-auto`}>
       {children}
     </div>
   );
