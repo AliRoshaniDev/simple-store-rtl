81316938bde3019e51270a059fd41f3ef1c74d77
Ali Moallem <AliMoallem27@gmail.com>
2021-07-10 16:55:09 +0430

    Modify useAuh hook with new authentication

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,18 +1,19 @@
-import { useContext } from "react";
-import { AuthContext, SetAuthContext } from "../providers/AuthProvider";
-import netlifyIdentity from "netlify-identity-widget";
+import { useIdentityContext } from "react-netlify-identity";
 
 export default function useAuth() {
-  const authData = useContext(AuthContext);
-  const setAuthData = useContext(SetAuthContext);
+  const { signupUser, loginUser, isLoggedIn, user, logoutUser } = useIdentityContext();
 
-  function open(tabName: "login" | "signup") {
-    netlifyIdentity.open(tabName);
+  function login(email: string, password: string, remember: boolean = true) {
+    loginUser(email, password, remember);
+  }
+
+  function signup(email: string, password: string, data: Object = {}, directLogin: boolean = true) {
+    signupUser(email, password, data, directLogin);
   }
 
   function logout() {
-    netlifyIdentity.logout();
+    logoutUser();
   }
 
-  return { authData, setAuthData, open, logout };
+  return { login, signup, logout, isLoggedIn, user };
 }
