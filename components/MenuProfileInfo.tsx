22b7c700213aa392ca56c04e5c1913bdaf5f927b
Ali Moallem <AliMoallem27@gmail.com>
2021-07-10 16:56:37 +0430

    Rename MenuUserInfo to MEnuProfileInfo

--- components/MenuProfileInfo.tsx
+++ components/MenuProfileInfo.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import useAuth from "../hooks/useAuth";
 import Btn from "./Btn";
 
-export default function MenuUserInfo() {
-  const { authData, logout } = useAuth();
+export default function MenuProfileInfo() {
+  const { user, logout } = useAuth();
 
   return (
     <div className="max-h-60 overflow-auto">
@@ -13,13 +13,13 @@ export default function MenuUserInfo() {
             <Image className="object-cover h-14 w-full rounded-lg" src="/images/others/default-profile.png" width={56} height={56} alt="prifle picture" />
           </figure>
           <div className="w-userItemText flex flex-col pr-2">
-            <span className="whitespace-nowrap text-gray-700 overflow-hidden font-vazir-latin">{authData!.user_metadata.full_name}</span>
-            <span className="text-gray-400 font-vazir-lati">{authData!.email}</span>
+            <span className="whitespace-nowrap text-gray-700 overflow-hidden font-vazir-latin">{user!.user_metadata.full_name}</span>
+            <span className="text-gray-400 font-vazir-lati">{user!.email}</span>
           </div>
         </div>
       </div>
       <div className="pt-2 border-t-2 border-gray-100">
-        <Btn onClick={() => logout()} text="خروج از حساب" type="secondary" size="full" />
+        <Btn onClick={() => logout()} text="خروج از حساب" type="secondary" size="w-full" />
       </div>
     </div>
   );
