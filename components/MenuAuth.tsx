a641d5c1c99e61a7d572c8fd83c089890c1256f9
Ali Moallem <AliMoallem27@gmail.com>
2021-07-12 03:25:07 +0430

    Modify UI for MenuAuth and MenuLoading

--- components/MenuAuth.tsx
+++ components/MenuAuth.tsx
@@ -6,13 +6,12 @@ import LoginInputList from "./LoginInputList";
 import SignupInputList from "./SignupInputList";
 
 export default function MenuAuth() {
-  const { login, signup } = useAuth();
   const [userStatus, setUserStatus] = useState<"login" | "signup" | null>(null);
 
   return (
     <div className="overflow-auto">
       <div className="min-h-48 text-right w-full whitespace-no-wrap rounded-lg text-sm md:text-base text-gray-600 hover:text-gray-800">
-        <div className="flex flex-col py-2 h-full">
+        <div className="flex flex-col py-2 pb-0 h-full">
           <div className="flex flex-col items-center">
             {userStatus === null && <EmojiAndText text="لطفا وارد شوید یا ثبت نام کنید" emoji="/images/icons/smile.png" />}
             {userStatus !== null && <Btn onClick={() => setUserStatus(null)} icon="/images/..." type="secondary" size="w-10" optionalStyle="mr-auto" noBorder={true} />}
