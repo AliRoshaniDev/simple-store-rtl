02a6e709082f9ef4d2f8dd091b37e4f70e2b331a
Ali Moallem <AliMoallem27@gmail.com>
2021-07-21 02:45:28 +0430

    Update NavBarCart

--- components/NavBarCart.tsx
+++ components/NavBarCart.tsx
@@ -9,18 +9,18 @@ import MenuEmptyCart from "./MenuEmptyCart";
 import MenuFillCart from "./MenuFillCart";
 
 export default function NavBarCart() {
-  const [parentElement, childElement, cartIsHover] = useFocusElement();
+  const [parentElement, childElement, cartIsFocus] = useFocusElement();
   const cartData = useCartData();
 
   return (
     <div ref={parentElement as LegacyRef<HTMLDivElement>} className="justify-self-start h-full cursor-pointer py-2 xl:py-3 relative">
-      <div className={`flex items-center relative h-full bg-transparent hover:bg-mycolor-light ${cartIsHover && "bg-mycolor-light"} rounded-lg transition duration-300 ease-in-out px-3`}>
+      <div className={`flex items-center relative h-full bg-transparent hover:bg-mycolor-light ${cartIsFocus && "bg-mycolor-light"} rounded-lg transition duration-300 ease-in-out px-3`}>
         {cartData && <Badge number={cartData.length} size="big" />}
         <span className="mr-2 hidden lg:inline">سبد خرید</span>
         <ReactSVG src="/images/icons/cart.svg" />
       </div>
       <div ref={childElement as LegacyRef<HTMLDivElement>}>
-        <DropMenu hidden={!cartIsHover} xAdjustment="top-13 -left-2" widthAdjustment="w-72 sm:w-80">
+        <DropMenu hidden={!cartIsFocus} xAdjustment="top-13 -left-2" widthAdjustment="w-72 sm:w-80">
           {!cartData || cartData.length === 0 ? <MenuEmptyCart /> : <MenuFillCart />}
         </DropMenu>
       </div>
