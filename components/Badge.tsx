4a79429303b78e4b1481466e9d907f4460f4d31d
Ali Moallem <AliMoallem27@gmail.com>
2021-07-21 02:36:56 +0430

    update Badge component

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -2,5 +2,5 @@ import { BadgeInputInterface } from "../types/index";
 
 export default function Badge(props: BadgeInputInterface) {
   const { size, number } = props;
-  return <div className={`${size === "big" ? "w-5 h-5" : "w-3 h-3"} absolute top-0 right-0 pt-0.5 bg-mycolor-dark text-xs rounded-full text-white flex items-center justify-center`}>{number}</div>;
+  return <div className={`${size === "big" ? "w-5 h-5" : "w-3 h-3"} absolute top-0 right-0 pt-0.5 bg-mycolor-dark text-xs rounded-full text-white flex items-center justify-center`}>{number ? number : ""}</div>;
 }
