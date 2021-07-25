9eeb2f095ab876e1947748184d25d6d2665d1736
Ali Moallem <AliMoallem27@gmail.com>
2021-07-25 13:39:16 +0430

    Update CartProvider

--- providers/CartProvider.tsx
+++ providers/CartProvider.tsx
@@ -1,19 +1,19 @@
 import { createContext } from "react";
 import useCartState from "../hooks/useCartState";
-import { ChildrenType, CartDataType } from "../types/index";
+import { ChildrenType, CartItemType, ActionCartType } from "../types/index";
 import { SetStateAction, Dispatch } from "react";
 
-export const CartContext = createContext<CartDataType | undefined>(undefined);
-export const SetCartContext = createContext<Dispatch<SetStateAction<CartDataType>> | undefined>(undefined);
+export const CartContext = createContext<CartItemType[] | null>(null);
+export const SetCartContext = createContext<Dispatch<ActionCartType> | null>(null);
 
 export default function CartProvider(props: ChildrenType) {
   const { children } = props;
 
-  let [cartData, setCartData] = useCartState();
+  let [cartData, dispatch] = useCartState();
 
   return (
     <CartContext.Provider value={cartData}>
-      <SetCartContext.Provider value={setCartData}>{children}</SetCartContext.Provider>
+      <SetCartContext.Provider value={dispatch}>{children}</SetCartContext.Provider>
     </CartContext.Provider>
   );
 }
