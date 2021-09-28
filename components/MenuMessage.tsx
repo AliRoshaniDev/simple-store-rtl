e6eed627d63ea7fe39cc6043baf597043a914c00
Ali Moallem <76528174+AliMoallem27@users.noreply.github.com>
2021-09-29 01:49:33 +0330

    Update MenuMessage.tsx

--- components/MenuMessage.tsx
+++ components/MenuMessage.tsx
@@ -12,7 +12,7 @@ export default function MenuMessage(props: { icon: string; text: string; spin?: 
 
   return (
     <div>
-      {authData.authStatus !== "LOADING" && <Btn onClick={handleCloseClick} icon="/images/icons/close.svg" type="secondary" width="w-10" buttonStyle="mr-auto" noBorder={true} />}
+      {authData.authStatus !== "LOADING" && <Btn onClick={handleCloseClick} icon="/images/icons/close.svg" type="secondary" width="w-10" buttonStyle="mr-auto" />}
       <div className="min-h-48 flex flex-col items-center justify-center">
         <ReactSVG src={icon} className={`w-12 h-12 fill-current text-mycolor-dark ${spin && "animate-spin"}`} />
         <span className="mt-6 text-sm md:text-base text-gray-600">{text}</span>
