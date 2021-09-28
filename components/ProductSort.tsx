8056ff0dd6c7d018ba139244eefb34cbf049f403
Ali Moallem <AliMoallem27@gmail.com>
2021-09-28 18:25:34 +0330

    Refactor Sort

--- components/ProductSort.tsx
+++ components/ProductSort.tsx
@@ -15,25 +15,17 @@ export default function ProductSort() {
     applyOneFilter("_sort", sortValue);
   }
 
-  function getCheckedValue(status: "price:ASC" | "price:DESC"): boolean {
-    if (queryObject["_sort"] === status) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
   return (
     <div className="relative h-10">
       <div ref={parentElement as LegacyRef<HTMLDivElement>} className={`flex flex-row-reverse items-center p-2 h-10 rounded-lg bg-white bg-transparent border-2 border-gray-300 ${itemIsFocus ? "border-mycolor-dark" : ""} hover:border-mycolor-dark hover:bg-mycolor-light ${itemIsFocus && "bg-mycolor-light"} cursor-pointer transition duration-200 ease-in-out`}>
-        {(getCheckedValue("price:DESC") || getCheckedValue("price:ASC")) && <Badge size="small" />}
+        {(queryObject["_sort"] === "price:DESC" || queryObject["_sort"] === "price:ASC") && <Badge size="small" />}
         <ReactSVG src="/images/icons/sort.svg" className="w-4" />
         <span className="mr-2 ml-1">مرتب سازی</span>
       </div>
       <div ref={childElement as LegacyRef<HTMLDivElement>}>
         <DropMenu hidden={!itemIsFocus} xAdjustment="top-10 -right-3" widthAdjustment="w-max">
-          <ProductMenuCheck text="از قیمت کم به زیاد" checked={getCheckedValue("price:ASC")} onChange={() => handleClickSort("price:ASC")} />
-          <ProductMenuCheck text="از قیمت زیاد به کم" checked={getCheckedValue("price:DESC")} onChange={() => handleClickSort("price:DESC")} />
+          <ProductMenuCheck text="از قیمت کم به زیاد" checked={queryObject["_sort"] === "price:ASC"} onChange={() => handleClickSort("price:ASC")} />
+          <ProductMenuCheck text="از قیمت زیاد به کم" checked={queryObject["_sort"] === "price:DESC"} onChange={() => handleClickSort("price:DESC")} />
         </DropMenu>
       </div>
     </div>
